4c9e55626427fd2a3fc4f8f3f723fa59ac610143
Jet <taijunjet@hotmail.com>
2022-10-19 18:13:23 +0100

    add seeding test

--- test/dm_control_test.py
+++ test/dm_control_test.py
@@ -1,5 +1,6 @@
 import inspect
 
+import numpy as np
 import pytest
 from dm_control import suite
 from dm_control.suite import (acrobot, ball_in_cup, cartpole, cheetah, dog,
@@ -17,6 +18,7 @@ _DOMAINS = {
     if inspect.ismodule(module) and hasattr(module, "SUITE")
 }
 
+
 @pytest.mark.parametrize("domain_name", _DOMAINS.keys())
 def test_all_envs(domain_name):
     # for each possible task in the domain:
@@ -33,6 +35,28 @@ def test_all_envs(domain_name):
         # run until termination
         while not term and not trunc:
             obs, rew, term, trunc, info = env.step(env.action_space.sample())
+
+
+def test_seeding():
+    # load envs
+    env1 = suite.load("hopper", "stand")
+    env2 = suite.load("hopper", "stand")
+
+    # convert the environment
+    env1 = dm_control_wrapper(env1, render_mode="rgb_array")
+    env2 = dm_control_wrapper(env2, render_mode="rgb_array")
+    env1.reset(seed=42)
+    env2.reset(seed=42)
+
+    for i in range(100):
+        returns1 = env1.step(env1.action_space.sample())
+        returns2 = env2.step(env2.action_space.sample())
+
+        for stuff1, stuff2 in zip(returns1, returns2):
+            if isinstance(stuff1, bool):
+                assert stuff1 == stuff2, f"Incorrect returns on iteration {i}."
+            elif isinstance(stuff1, np.ndarray):
+                assert (stuff1 == stuff2).all(), f"Incorrect returns on iteration {i}."
 
 
 def test_render():
