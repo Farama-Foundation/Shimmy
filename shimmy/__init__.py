81bab60dcea2c329e968f031d2142fb931f8b591
StringTheory <mark.m.towers@gmail.com>
2022-11-10 19:59:07 +0000

    Add install [all] to build and add try except for `__init__.py` installs

--- shimmy/__init__.py
+++ shimmy/__init__.py
@@ -1,8 +1,16 @@
 """API for converting popular non-gymnasium environments to a gymnasium compatible environment."""
 
-from shimmy.dm_control_compatibility import (
-    DmControlCompatibility as DmControlCompatibilityV0,
-)
-from shimmy.openspiel_wrapper import OpenspielWrapper as OpenspielWrapperV0
+try:
+    from shimmy.dm_control_compatibility import (
+        DmControlCompatibility as DmControlCompatibilityV0,
+    )
+except ImportError:
+    pass
+
+try:
+    from shimmy.openspiel_wrapper import OpenspielWrapper as OpenspielWrapperV0
+except ImportError:
+    pass
+
 
 __version__ = "0.0.1a"
