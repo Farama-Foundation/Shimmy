1319b195def72b6aa6667d91bed2763532a5ce7c
Mark Towers <mark.m.towers@gmail.com>
2022-11-10 22:26:57 +0000

    Add docker for testing (#12)

--- setup.py
+++ setup.py
@@ -33,9 +33,10 @@ version = get_version()
 header_count, long_description = get_description()
 
 extras = {
-    "dm-control": ["dm-control>=1.0.8"],
+    # "imageio" should be "gymnasium[mujoco]>=0.26" but there are install conflicts
+    "dm-control": ["dm-control>=1.0.8", "imageio"],
     "openspiel": ["open_spiel>=1.2", "pettingzoo>=1.22"],
-    "ale-py": ["ale-py~=0.8.0"],
+    "atari": ["ale-py~=0.8.0"],
 }
 extras["all"] = list({lib for libs in extras.values() for lib in libs})
 extras["testing"] = extras["all"] + [
