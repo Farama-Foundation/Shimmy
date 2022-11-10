979662fb42fa4e6b087b757721ad7458a31cc2b3
StringTheory <mark.m.towers@gmail.com>
2022-11-10 17:06:48 +0000

    merge with origin/main

--- shimmy/registration.py
+++ shimmy/registration.py
@@ -5,98 +5,16 @@ from collections import defaultdict
 from functools import partial
 from typing import Any, Callable, Mapping, NamedTuple, Sequence
 
-from ale_py.roms import utils as rom_utils
 import numpy as np
+from ale_py.roms import utils as rom_utils
 from gymnasium.envs.registration import register
 
 from shimmy.dm_control_compatibility import DmControlCompatibility
-
 from shimmy.utils.envs_configs import (
     ALL_ATARI_GAMES,
-    DM_CONTROL_ENVS,
+    DM_CONTROL_MANIPULATION_ENVS,
+    DM_CONTROL_SUITE_ENVS,
     LEGACY_ATARI_GAMES,
-)
-
-DM_CONTROL_SUITE_ENVS = (
-    ("acrobot", "swingup"),
-    ("acrobot", "swingup_sparse"),
-    ("ball_in_cup", "catch"),
-    ("cartpole", "balance"),
-    ("cartpole", "balance_sparse"),
-    ("cartpole", "swingup"),
-    ("cartpole", "swingup_sparse"),
-    ("cartpole", "two_poles"),
-    ("cartpole", "three_poles"),
-    ("cheetah", "run"),
-    ("dog", "stand"),
-    ("dog", "walk"),
-    ("dog", "trot"),
-    ("dog", "run"),
-    ("dog", "fetch"),
-    ("finger", "spin"),
-    ("finger", "turn_easy"),
-    ("finger", "turn_hard"),
-    ("fish", "upright"),
-    ("fish", "swim"),
-    ("hopper", "stand"),
-    ("hopper", "hop"),
-    ("humanoid", "stand"),
-    ("humanoid", "walk"),
-    ("humanoid", "run"),
-    ("humanoid", "run_pure_state"),
-    ("humanoid_CMU", "stand"),
-    ("humanoid_CMU", "run"),
-    ("lqr", "lqr_2_1"),
-    ("lqr", "lqr_6_2"),
-    ("manipulator", "bring_ball"),
-    ("manipulator", "bring_peg"),
-    ("manipulator", "insert_ball"),
-    ("manipulator", "insert_peg"),
-    ("pendulum", "swingup"),
-    ("point_mass", "easy"),
-    ("point_mass", "hard"),
-    ("quadruped", "walk"),
-    ("quadruped", "run"),
-    ("quadruped", "escape"),
-    ("quadruped", "fetch"),
-    ("reacher", "easy"),
-    ("reacher", "hard"),
-    ("stacker", "stack_2"),
-    ("stacker", "stack_4"),
-    ("swimmer", "swimmer6"),
-    ("swimmer", "swimmer15"),
-    ("walker", "stand"),
-    ("walker", "walk"),
-    ("walker", "run"),
-)
-
-
-DM_CONTROL_MANIPULATION_ENVS = (
-    "stack_2_bricks_features",
-    "stack_2_bricks_vision",
-    "stack_2_bricks_moveable_base_features",
-    "stack_2_bricks_moveable_base_vision",
-    "stack_3_bricks_features",
-    "stack_3_bricks_vision",
-    "stack_3_bricks_random_order_features",
-    "stack_2_of_3_bricks_random_order_features",
-    "stack_2_of_3_bricks_random_order_vision",
-    "reassemble_3_bricks_fixed_order_features",
-    "reassemble_3_bricks_fixed_order_vision",
-    "reassemble_5_bricks_random_order_features",
-    "reassemble_5_bricks_random_order_vision",
-    "lift_brick_features",
-    "lift_brick_vision",
-    "lift_large_box_features",
-    "lift_large_box_vision",
-    "place_brick_features",
-    "place_brick_vision",
-    "place_cradle_features",
-    "place_cradle_vision",
-    "reach_duplo_features",
-    "reach_duplo_vision",
-    "reach_site_features",
-    "reach_site_vision",
 )
 
 
@@ -147,11 +65,7 @@ def _register_dm_control_envs():
     # Register all example locomotion environments
     # Listed in https://github.com/deepmind/dm_control/blob/main/dm_control/locomotion/examples/examples_test.py
     from dm_control import composer
-    from dm_control.locomotion.examples import (
-        basic_cmu_2019,
-        basic_rodent_2020,
-        cmu_2020_tracking,
-    )
+    from dm_control.locomotion.examples import basic_cmu_2019, basic_rodent_2020
 
     def _make_dm_control_example_locomotion_env(
         env_fn: Callable[[np.random.RandomState | None], composer.Environment],
