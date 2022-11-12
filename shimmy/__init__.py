bd2e52a6e0616409f9b153158210b4ab85a9d1fc
Mark Towers <mark.m.towers@gmail.com>
2022-11-12 13:15:33 +0000

    Update version

--- shimmy/__init__.py
+++ shimmy/__init__.py
@@ -1,5 +1,8 @@
 """API for converting popular non-gymnasium environments to a gymnasium compatible environment."""
 
+__version__ = "0.1.0"
+
+
 try:
     from shimmy.dm_control_compatibility import (
         DmControlCompatibility as DmControlCompatibilityV0,
@@ -11,6 +14,3 @@ try:
     from shimmy.openspiel_wrapper import OpenspielWrapper as OpenspielWrapperV0
 except ImportError:
     pass
-
-
-__version__ = "0.0.1a"
