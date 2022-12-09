22adf471a756725fbcdec629395ef72db6a107a8
jjshoots <taijunjet@hotmail.com>
2022-12-09 17:44:22 +0000

    move import in heheh

--- tests/test_dm_lab.py
+++ tests/test_dm_lab.py
@@ -1,6 +1,5 @@
 """Tests the multi-agent dm-control soccer environment."""
 
-import deepmind_lab
 import gymnasium
 import pytest
 from gymnasium.utils.env_checker import check_env
@@ -11,6 +10,8 @@ from shimmy.dm_lab_compatibility import DmLabCompatibilityV0
 @pytest.mark.skip(reason="no way of currently testing this")
 def test_check_env():
     """Check that environment pass the gym check_env."""
+    import deepmind_lab
+
     observations = ["RGBD"]
     config = {"width": "640", "height": "480", "botCount": "2"}
     renderer = "hardware"
