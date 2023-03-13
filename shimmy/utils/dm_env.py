f7553f8ffd5df9c23f7714a92c8d9000f9302909
elliottower <elliot@elliottower.com>
2023-03-13 19:01:05 -0400

    Separated dm_control and meltingpot utils (fix CI issue with conversion util)

--- shimmy/utils/dm_env.py
+++ shimmy/utils/dm_env.py
@@ -1,6 +1,4 @@
 """Utility functions for the compatibility wrappers."""
-
-# pyright:  reportGeneralTypeIssues=false
 from __future__ import annotations
 
 import copy
@@ -9,44 +7,41 @@ from typing import Any
 
 import dm_env
 import numpy as np
-import tree
-
-# from dm_env.specs import Array, BoundedArray, DiscreteArray
+from dm_env.specs import Array, BoundedArray, DiscreteArray
 from gymnasium import spaces
 
 
-def dm_spec2gym_space(spec: tree.Structure[dm_env.specs.Array]) -> spaces.Space:
-    """Converts a dm_env nested structure of specs to a Gymnasium Space.
-
-    BoundedArray is converted to Box Gymnasium spaces. DiscreteArray is converted to
-    Discrete Gymnasium spaces. Using Tuple and Dict spaces recursively as needed.
-
-    Args:
-      spec: The nested structure of specs
-
-    Returns:
-      The Gymnasium space corresponding to the given spec.
-    """
-    if isinstance(spec, dm_env.specs.DiscreteArray):
-        return spaces.Discrete(spec.num_values)
-    elif isinstance(spec, dm_env.specs.BoundedArray):
-        return spaces.Box(spec.minimum, spec.maximum, spec.shape, spec.dtype)
-    elif isinstance(spec, dm_env.specs.Array):
-        if np.issubdtype(spec.dtype, np.floating):
-            return spaces.Box(-np.inf, np.inf, spec.shape, spec.dtype)
-        elif np.issubdtype(spec.dtype, np.integer):
-            info = np.iinfo(spec.dtype)
-            return spaces.Box(info.min, info.max, spec.shape, spec.dtype)
-        elif np.issubtype(spec.dtype, bool):
-            return spaces.Box(int(0), int(1), spec.shape, spec.dtype)
+def dm_spec2gym_space(spec) -> spaces.Space[Any]:
+    """Converts a dm_env spec to a gymnasium space."""
+    if isinstance(spec, (OrderedDict, dict)):
+        return spaces.Dict(
+            {key: dm_spec2gym_space(value) for key, value in copy.copy(spec).items()}
+        )
+    # not possible to use isinstance due to inheritance
+    elif type(spec) is BoundedArray:
+        low = np.broadcast_to(spec.minimum, spec.shape)
+        high = np.broadcast_to(spec.maximum, spec.shape)
+        return spaces.Box(low=low, high=high, shape=spec.shape, dtype=spec.dtype)
+    elif type(spec) is Array:
+        if np.issubdtype(spec.dtype, np.integer):
+            low = np.iinfo(spec.dtype).min
+            high = np.iinfo(spec.dtype).max
+        elif np.issubdtype(spec.dtype, np.inexact):
+            low = float("-inf")
+            high = float("inf")
+        elif spec.dtype == "bool":
+            low = int(0)
+            high = int(1)
         else:
-            raise NotImplementedError(f"Unsupported dtype {spec.dtype}")
-    elif isinstance(spec, (list, tuple)):
-        return spaces.Tuple([dm_spec2gym_space(s) for s in spec])
-    elif isinstance(spec, (OrderedDict, dict)):
-        return spaces.Dict({key: dm_spec2gym_space(s) for key, s in spec.items()})
+            raise ValueError(f"Unknown dtype {spec.dtype} for spec {spec}.")
+
+        return spaces.Box(low=low, high=high, shape=spec.shape, dtype=spec.dtype)
+    elif type(spec) is DiscreteArray:
+        return spaces.Discrete(spec.num_values)
     else:
-        raise ValueError(f"Unexpected spec of type {type(spec)}: {spec}")
+        raise NotImplementedError(
+            f"Cannot convert dm_spec to gymnasium space, unknown spec: {spec}, please report."
+        )
 
 
 def dm_obs2gym_obs(obs: dm_env.TimeStep.observation) -> np.ndarray | dict[str, Any]:
--- shimmy/utils/meltingpot.py
+++ shimmy/utils/meltingpot.py
@@ -3,15 +3,48 @@
 # pyright:  reportGeneralTypeIssues=false
 from typing import Any, Mapping, OrderedDict
 
+import numpy as np
+import tree
 import dm_env
 from gymnasium import spaces
 from pettingzoo.utils.env import ObsDict
 
-from shimmy.utils.dm_env import dm_spec2gym_space
 
 PLAYER_STR_FORMAT = "player_{index}"
 _WORLD_PREFIX = "WORLD."
 
+def dm_spec2gym_space(spec: tree.Structure[dm_env.specs.Array]) -> spaces.Space:
+    """Converts a dm_env nested structure of specs to a Gymnasium Space.
+
+    BoundedArray is converted to Box Gymnasium spaces. DiscreteArray is converted to
+    Discrete Gymnasium spaces. Using Tuple and Dict spaces recursively as needed.
+
+    Args:
+      spec: The nested structure of specs
+
+    Returns:
+      The Gymnasium space corresponding to the given spec.
+    """
+    if isinstance(spec, dm_env.specs.DiscreteArray):
+        return spaces.Discrete(spec.num_values)
+    elif isinstance(spec, dm_env.specs.BoundedArray):
+        return spaces.Box(spec.minimum, spec.maximum, spec.shape, spec.dtype)
+    elif isinstance(spec, dm_env.specs.Array):
+        if np.issubdtype(spec.dtype, np.floating):
+            return spaces.Box(-np.inf, np.inf, spec.shape, spec.dtype)
+        elif np.issubdtype(spec.dtype, np.integer):
+            info = np.iinfo(spec.dtype)
+            return spaces.Box(info.min, info.max, spec.shape, spec.dtype)
+        elif np.issubtype(spec.dtype, bool):
+            return spaces.Box(int(0), int(1), spec.shape, spec.dtype)
+        else:
+            raise NotImplementedError(f"Unsupported dtype {spec.dtype}")
+    elif isinstance(spec, (list, tuple)):
+        return spaces.Tuple([dm_spec2gym_space(s) for s in spec])
+    elif isinstance(spec, (OrderedDict, dict)):
+        return spaces.Dict({key: dm_spec2gym_space(s) for key, s in spec.items()})
+    else:
+        raise ValueError(f"Unexpected spec of type {type(spec)}: {spec}")
 
 def timestep_to_observations(timestep: dm_env.TimeStep) -> ObsDict:
     """Extracts Gymnasium-compatible observations from a melting pot timestep.
