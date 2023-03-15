c0d57beca73285d74d5dc4b04443b1544733b1b5
elliottower <elliot@elliottower.com>
2023-03-15 16:49:52 -0400

    Moved autorom dep from testing to atari (can sometimes cause CI failures for unrealted envs)

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ header_count, long_description = get_description()
 
 extras = {
     "gym": ["gym>=0.21"],
-    "atari": ["ale-py~=0.8.1"],
+    "atari": ["ale-py~=0.8.1", "autorom[accept-rom-license]~=0.4.2",],
     # "imageio" should be "gymnasium[mujoco]>=0.26" but there are install conflicts
     "dm-control": ["dm-control>=1.0.10", "imageio", "h5py>=3.7.0"],
     "dm-control-multi-agent": ["dm-control>=1.0.10", "pettingzoo>=1.22"],
@@ -45,7 +45,6 @@ extras["all"] = list({lib for libs in extras.values() for lib in libs})
 extras["testing"] = [
     "pytest==7.1.3",
     "pillow>=9.3.0",
-    "autorom[accept-rom-license]~=0.4.2",
 ]
 
 setup(
